7b53858e87d11d289eb0d43f80a11a6179ea52a2
Clay <clayp31827@outlook.com>
2024-01-29 18:10:23 -0500

    First commit for Powers Table

--- Powers Table Lab.py
+++ Powers Table Lab.py
@@ -1,12 +1,13 @@
-x = int(input("Enter an integer:"))
-k = [1,x]
-print(k[1:x])
-y = range(1,x+1)
-print(y[1:x])
-for num in y:
-    print(num)
-    num2 = num * num
-for num2 in y:
-    print(num2)
-operation = ["Number", "Squared", "Cubed"]
-print(operation)
+while True:
+    x = int(input("Enter an integer:"))
+    y = range(1,x+1)
+    operation = ["Number", "Squared", "Cubed"]
+    dash = ["="*6, "="*7, "="*5]
+    print(operation)
+    print(dash)
+    for num in y:
+        print("   ", num, "        ", num*num, "        ", num*num*num)
+    response = input("Would you like to continue?")
+    if response != "Yes":
+     print("Thank you!")
+     break
